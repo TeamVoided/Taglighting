c82074e3d37fd41b5bd26339a5b60bcdde8821e0
TheEnderCore <theendercore@gmail.com>
2025-05-22 15:56:20 +0300

    - tag holder helpers

--- src/main/kotlin/org/teamvoided/taglighting/misc/Helpers.kt
+++ src/main/kotlin/org/teamvoided/taglighting/misc/Helpers.kt
@@ -3,6 +3,8 @@
 package org.teamvoided.taglighting.misc
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider
+import net.minecraft.registry.Holder
 import net.minecraft.registry.HolderLookup
 import net.minecraft.registry.Registry
 import net.minecraft.registry.RegistryKey
@@ -17,3 +19,14 @@ typealias FutureProvider = CompletableFuture<HolderLookup.Provider>
 
 fun <T, Y : Registry<T>> RegistryKey<Y>.key(id: Identifier): RegistryKey<T> = RegistryKey.of(this, id)
 fun <T, Y : Registry<T>> RegistryKey<Y>.tag(id: Identifier): TagKey<T> = TagKey.of(this, id)
+
+
+fun <T> FabricTagProvider<T>.FabricTagBuilder.add(holder: Holder<T>): FabricTagProvider<T>.FabricTagBuilder {
+    this.add(holder.value())
+    return this
+}
+
+fun <T> FabricTagProvider<T>.FabricTagBuilder.add(vararg holders: Holder<T>): FabricTagProvider<T>.FabricTagBuilder {
+    for (holder in holders) this.add(holder)
+    return this
+}
