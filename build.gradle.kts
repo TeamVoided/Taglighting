8db74ad8f2b4dde35dd72d9d2a4edefea75af5f6
TheEnderCore <theendercore@gmail.com>
2025-05-24 16:08:13 +0300

    - updated iridium

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,13 +12,6 @@ plugins {
     alias(libs.plugins.iridium.upload)
 }
 
-group = property("maven_group")!!
-version = property("mod_version")!!
-base.archivesName.set(modSettings.modId())
-
-val modrinth_id: String? by project
-val curse_id: String? by project
-
 repositories {
     maven("https://teamvoided.org/releases")
     maven("https://teamvoided.org/snapshots")
@@ -42,21 +35,35 @@ modSettings {
 }
 
 dependencies {
-
-    modImplementation(fileTree("libs"))
+    // QoL
     modImplementation(libs.modmenu)
-
     modCompileOnly("${libs.emi.get()}:api")
     modLocalRuntime(libs.emi)
-
-    // Testing
+    // Dev
+    modImplementation(libs.devin)
     modImplementation(libs.creative.works)
+    modImplementation(libs.fzzy.config)
     modImplementation(libs.imguimc)
 }
 
+val username = "vDev"
+val uuid: String? = null
 loom {
     splitEnvironmentSourceSets()
     runs {
+        named("client") {
+            programArgs("--username", username)
+            uuid?.let { programArgs("--uuid", uuid) }
+        }
+
+        create("TestWorld") {
+            client()
+            ideConfigGenerated(true)
+            runDir("run")
+            programArgs("--quickPlaySingleplayer", "test", "--username", username)
+            uuid?.let { programArgs("--uuid", uuid) }
+        }
+
         create("DataGen") {
             client()
             ideConfigGenerated(true)
@@ -64,13 +71,6 @@ loom {
             vmArg("-Dfabric-api.datagen.output-dir=${file("src/main/generated")}")
             vmArg("-Dfabric-api.datagen.modid=${modSettings.modId()}")
             runDir("build/datagen")
-        }
-
-        create("TestWorld") {
-            client()
-            ideConfigGenerated(true)
-            runDir("run")
-            programArgs("--quickPlaySingleplayer", "test")
         }
     }
 }
@@ -110,8 +110,8 @@ publishScript {
 
 uploadConfig {
 //    debugMode = true
-    modrinthId = modrinth_id
-    curseId = curse_id
+    modrinthId = "x"
+    curseId = "0"
 
     // FabricApi
     modrinthDependency("P7dR8mSH", uploadConfig.REQUIRED)
